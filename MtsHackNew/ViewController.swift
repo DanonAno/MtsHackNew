eab5eb4f4792429d2769a2f1e33dcd4b165b7c32
Daniil Luparevich <daniilluparevich@gmail.com>
2023-03-29 19:00:59 +0500

    Initial Commit

---
+++ MtsHackNew/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MtsHackNew
+//
+//  Created by  Даниил on 29.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
